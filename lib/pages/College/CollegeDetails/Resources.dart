108708156907b3e943b8c636f3b7751f324faab5
hubvue <1191340528@qq.com>
2020-03-06 01:33:32 +0800

    feat: 资料&简章&调起百度网盘

--- lib/pages/College/CollegeDetails/Resources.dart
+++ lib/pages/College/CollegeDetails/Resources.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Resources extends StatefulWidget {
   final String collegeName;
@@ -7,21 +9,174 @@ class Resources extends StatefulWidget {
 }
 
 class _ResourcesState extends State<Resources> {
+  List<Map> courceResources = [
+    {
+      'cource': '数据结构',
+      'resource': [
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        },
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        },
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        }
+      ]
+    },
+    {
+      'cource': '数据结构',
+      'resource': [
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        }
+      ]
+    },
+    {
+      'cource': '数据结构',
+      'resource': [
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        }
+      ]
+    },
+    {
+      'cource': '数据结构',
+      'resource': [
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        }
+      ]
+    },
+    {
+      'cource': '数据结构',
+      'resource': [
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        }
+      ]
+    },
+    {
+      'cource': '数据结构',
+      'resource': [
+        {
+          'url': 'https://pan.baidu.com/s/1_JgZFUTxgyHbFtLIJpy4pA',
+          'ident': 'fi1t',
+        }
+      ]
+    },
+  ];
   @override
   void initState() {
     super.initState();
     fetchRequest();
   }
 
+  // 通过学校名称去拿资料
   Future<void> fetchRequest() async {}
+  Function callCoundDisk(String url) {
+    return () async {
+      try {
+        var result = await launch(url);
+        if (!result) {
+          Toast.show(
+            '打开百度网盘失败，请自行粘贴地址到浏览器中打开',
+            context,
+            duration: Toast.LENGTH_LONG,
+          );
+        }
+        print(result);
+      } catch (err) {
+        print(err);
+      }
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
+    num screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('真题资料'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        width: screenWidth,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: contentWidget(),
+          ),
+        ),
+      ),
     );
+  }
+
+  List<Widget> contentWidget() {
+    List<Widget> courcesWidget = courceResources
+        .map((cource) {
+          List<Widget> resources = cource['resource']
+              .map((source) {
+                return GestureDetector(
+                  onTap: callCoundDisk(source['url']),
+                  child: Container(
+                    padding: EdgeInsets.all(4),
+                    margin: EdgeInsets.only(bottom: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        SingleChildScrollView(
+                          child: SelectableText(
+                            '链接：${source['url']}',
+                          ),
+                        ),
+                        SelectableText('密码：${source['ident']}'),
+                      ],
+                    ),
+                  ),
+                );
+              })
+              .cast<Widget>()
+              .toList();
+          return Container(
+            color: Colors.blue[100],
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    cource['cource'],
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: resources,
+                  ),
+                ),
+              ],
+            ),
+          );
+        })
+        .cast<Widget>()
+        .toList();
+    return courcesWidget;
   }
 }
--- lib/pages/College/CollegeDetails/Totor.dart
+++ lib/pages/College/CollegeDetails/Totor.dart
@@ -7,7 +7,42 @@ class Totor extends StatefulWidget {
 }
 
 class _TotorState extends State<Totor> {
-  String platform;
+  List<Map> totorsInfo = [
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+    {'name': '王老师', 'number': '15036889002'},
+  ];
+  List<Widget> childrenWidget = [];
   @override
   void initState() {
     super.initState();
@@ -19,12 +54,73 @@ class _TotorState extends State<Totor> {
 
   @override
   Widget build(BuildContext context) {
+    num screenWidth = MediaQuery.of(context).size.width;
+    childrenWidget.add(titleWidget(screenWidth));
+    childrenWidget.addAll(contentWidget(screenWidth));
     return Scaffold(
       appBar: AppBar(
         title: Text('研究生导师'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        width: screenWidth,
+        padding: EdgeInsets.only(bottom: 16),
+        child: SingleChildScrollView(
+          child: Column(
+            children: childrenWidget,
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget titleWidget(num screenWidth) {
+    TextStyle titleStyle = TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    );
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            width: screenWidth / 2,
+            child: Center(child: Text('姓名', style: titleStyle)),
+          ),
+          Container(
+            width: screenWidth / 2,
+            child: Center(child: Text('联系方式', style: titleStyle)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<Widget> contentWidget(num screenWidth) {
+    List<Widget> totorsWidget = totorsInfo.map((totor) {
+      return Container(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 0.5, color: Colors.grey[200]),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              width: screenWidth / 2,
+              child: Center(child: Text(totor['name'])),
+            ),
+            Container(
+              width: screenWidth / 2,
+              child: Center(child: Text(totor['number'])),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+    return totorsWidget;
   }
 }
