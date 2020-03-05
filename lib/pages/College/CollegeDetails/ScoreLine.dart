6e03003853b70f9fc2d0a9e2645c224f611c875f
hubvue <1191340528@qq.com>
2020-03-06 01:49:34 +0800

    feat: 历年分数线+调剂采用内嵌webview的方式

--- lib/pages/College/CollegeDetails/ScoreLine.dart
+++ lib/pages/College/CollegeDetails/ScoreLine.dart
@@ -7,6 +7,14 @@ class ScoreLine extends StatefulWidget {
 }
 
 class _ScoreLineState extends State<ScoreLine> {
+  Map yearScoreLine = {
+    '2020': '386',
+    '2019': '386',
+    '2018': '386',
+    '2017': '386',
+    '2016': '386',
+    '2015': '386',
+  };
   @override
   void initState() {
     super.initState();
@@ -16,12 +24,57 @@ class _ScoreLineState extends State<ScoreLine> {
   Future<void> fetchRequest() async {}
   @override
   Widget build(BuildContext context) {
+    num screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('历年分数线'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: scoreLineContent(screenWidth),
+        ),
+      ),
     );
+  }
+
+  List<Widget> scoreLineContent(num screenWidth) {
+    return yearScoreLine.keys.map((year) {
+      return Container(
+        width: screenWidth,
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(width: 0.5, color: Colors.grey[200]),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              year,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '年初始分数线：',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              '${yearScoreLine[year]}分',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
   }
 }
